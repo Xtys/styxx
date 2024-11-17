8a66f1c85987021f71e157fb70a0e5a3025b179c
styx <styxmodz@gmail.com>
2024-11-18 07:40:17 +0800

    Update get_counts.php

--- get_counts.php
+++ get_counts.php
@@ -1,9 +1,7 @@
 <?php
 $counterFile = 'downloads_count.json';
-
 if (file_exists($counterFile)) {
-    $counts = json_decode(file_get_contents($counterFile), true);
-    echo json_encode($counts);
+    echo file_get_contents($counterFile);
 } else {
     echo json_encode([]);
 }
