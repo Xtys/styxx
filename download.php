f0c099464a6002c002ee8a56e2c7d30eef254051
styx <styxmodz@gmail.com>
2024-11-18 07:24:16 +0800

    Update download.php

--- download.php
+++ download.php
@@ -1,21 +1,28 @@
 <?php
+$file_key = $_GET['file'];
 $file_paths = array(
     "ako_tamaki_v2.3" => "https://icedrive.net/s/6zwYCYDyRZS8yb95YajXGfXbfx8u",
     "kurisu_makise_v1.3" => "https://icedrive.net/s/GtuPFkWDhxC3Q82kX57T9WZYt6Z7"
 );
 
-if (isset($_GET['file'])) {
-    $file_key = $_GET['file'];
+// Check if the file exists in the array and proceed to download
+if(array_key_exists($file_key, $file_paths)) {
+    $file = $file_paths[$file_key];
     
-    if (array_key_exists($file_key, $file_paths)) {
-        header("Location: " . $file_paths[$file_key]);
+    if (file_exists($file)) {
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename="' . basename($file) . '"');
+        header('Content-Transfer-Encoding: binary');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($file));
+        readfile($file);
         exit;
     } else {
-        echo "Invalid file request.";
-        exit;
+        echo "File not found.";
     }
+} else {
+    echo "Invalid file request.";
 }
-
-http_response_code(400);
-echo "No file specified.";
-?>
