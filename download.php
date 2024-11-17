9d7fcd280ca667217f55c1a5f50b1fb27141e2c0
styx <styxmodz@gmail.com>
2024-11-18 07:12:33 +0800

    Update download.php

--- download.php
+++ download.php
@@ -1,14 +1,17 @@
 <?php
+// Map the file key to actual file URLs
 $file_paths = array(
     "ako_tamaki_v2.3" => "https://icedrive.net/s/6zwYCYDyRZS8yb95YajXGfXbfx8u",
     "kurisu_makise_v1.3" => "https://icedrive.net/s/GtuPFkWDhxC3Q82kX57T9WZYt6Z7"
 );
 
 if (isset($_GET['file'])) {
-    $file_key = $_GET['file'];
+    $fileKey = $_GET['file'];
     
-    if (array_key_exists($file_key, $file_paths)) {
-        header("Location: " . $file_paths[$file_key]);
+    // Check if the file exists in the array
+    if (array_key_exists($fileKey, $file_paths)) {
+        // Redirect the user to the external Icedrive URL
+        header("Location: " . $file_paths[$fileKey]);
         exit;
     } else {
         echo "Invalid file request.";
